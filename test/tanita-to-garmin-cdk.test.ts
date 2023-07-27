166a5de91d31377ad5f5b0a033fd45d417d1ef01
Pablo Albaladejo <pablo@hireful.co.uk>
2023-07-27 11:33:08 +0200

    chore: initial commit

---
+++ test/tanita-to-garmin-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TanitaToGarminCdk from '../lib/tanita-to-garmin-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tanita-to-garmin-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TanitaToGarminCdk.TanitaToGarminCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
