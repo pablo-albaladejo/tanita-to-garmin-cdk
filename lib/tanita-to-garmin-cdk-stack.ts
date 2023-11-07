3560fa1c79b0b2a782d640acfe1892a0965c47f9
Pablo Albaladejo <pablo@hireful.co.uk>
2023-11-07 23:00:03 +0100

    chore: fix tanita function name

--- lib/tanita-to-garmin-cdk-stack.ts
+++ lib/tanita-to-garmin-cdk-stack.ts
@@ -1,13 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import fitToolkit from './fitToolkit';
-import tanitaToCsv from './tanitaToJson';
+import tanitaToJson from './tanitaToJson';
 
 export class TanitaToGarminCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const tanitaToCsvFunction = tanitaToCsv(this);
+    const tanitaToCsvFunction = tanitaToJson(this);
 
     const fitToolkitFunction = fitToolkit(this);
 
