b1ee7f35219c537d5c4f1f5c015f97d774c90d49
Pablo Albaladejo <pablo@hireful.co.uk>
2023-11-08 03:54:15 +0100

    feat: add s3 file upload at fittoolkit

--- lib/fitToolkit.ts
+++ lib/fitToolkit.ts
@@ -1,16 +1,41 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export default (scope: Construct): lambda.Function => {
+  const blockPublicAccess: s3.BlockPublicAccess = {
+    blockPublicAcls: false,
+    blockPublicPolicy: false,
+    ignorePublicAcls: false,
+    restrictPublicBuckets: false,
+  };
+  const fitFileS3Bucket = new s3.Bucket(scope, 'FitFileS3Bucket', {
+    removalPolicy: cdk.RemovalPolicy.DESTROY,
+  });
+  const s3PublicReadPolicy = new iam.PolicyStatement({
+    actions: ['s3:GetObject'],
+    resources: [`${fitFileS3Bucket.bucketArn}/*`],
+    effect: iam.Effect.ALLOW,
+    principals: [new iam.ArnPrincipal('*')],
+  });
+  fitFileS3Bucket.addToResourcePolicy(s3PublicReadPolicy);
+
   const fitToolkitFunction = new lambda.Function(scope, 'FitToolkitFunction', {
     runtime: lambda.Runtime.JAVA_11,
     handler: 'fit.toolkit.handler.WeightHandler::handleRequest',
     code: lambda.Code.fromAsset(
       'src/lambdas/java/fitToolkit/target/fit-toolkit-latest.jar'
     ),
-    timeout: cdk.Duration.seconds(60),
+    timeout: cdk.Duration.seconds(300),
+    memorySize: 512,
+    environment: {
+      BUCKET_NAME: fitFileS3Bucket.bucketName,
+    },
   });
+  fitFileS3Bucket.grantReadWrite(fitToolkitFunction);
+
   const fitToolkitFunctionUrl = fitToolkitFunction.addFunctionUrl({
     authType: lambda.FunctionUrlAuthType.NONE,
   });
--- src/lambdas/java/fitToolkit/src/main/java/fit/toolkit/handler/WeightHandler.java
+++ src/lambdas/java/fitToolkit/src/main/java/fit/toolkit/handler/WeightHandler.java
@@ -4,6 +4,9 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 import com.garmin.fit.*;
 import com.google.gson.Gson;
@@ -12,12 +15,8 @@ import com.google.gson.JsonObject;
 
 import java.io.File;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.time.ZonedDateTime;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -44,24 +43,12 @@ public class WeightHandler implements RequestHandler<APIGatewayProxyRequestEvent
 
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-        /*
-            [{
-                "date": "2023-07-27T12:34:56Z",
-                "weight": 70.5,
-                "body_fat": 22.3,
-                "body_water": 55.8,
-                "muscle_mass": 65.2,
-                "bmr": 1550.6,
-                "physique_rating": 4,
-                "visc_fat": 7,
-                "bone_mass": 3.2,
-                "metab_age": 32,
-                "bmi": 24.1
-            }] 
-        */
+        String bucketName = System.getenv("BUCKET_NAME");
+
         SimpleDateFormat fileNameFormater = new SimpleDateFormat("yyyyMMdd_kkmmss");
         String filename = "ws_" + fileNameFormater.format(new Date()) + ".fit";
-        FileEncoder encoder = new FileEncoder(new File("/tmp/", filename), Fit.ProtocolVersion.V1_0);
+        File file = new File("/tmp/", filename);
+        FileEncoder encoder = new FileEncoder(file, Fit.ProtocolVersion.V1_0);
 
         FileIdMesg fileIdMesg = new FileIdMesg();
         fileIdMesg.setType(com.garmin.fit.File.WEIGHT);
@@ -78,57 +65,22 @@ public class WeightHandler implements RequestHandler<APIGatewayProxyRequestEvent
         });
 
         encoder.close();
-        try {
-            String b64 = encodeFileToBase64Binary("/tmp/" + filename);
 
-            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
-            response.setStatusCode(200);
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+        PutObjectRequest request = new PutObjectRequest(bucketName, filename, file);
+        s3Client.putObject(request);
 
-            Map<String, String> headers = new HashMap<>();
-            headers.put("Content-Type", "application/octet-stream");
-            headers.put("X-Custom-Header", "application/octet-stream");
-            headers.put("Content-Disposition", "attachment; filename=\"" + filename + "\"");
-            response.setHeaders(headers);
+        String objectURL = s3Client.getUrl(bucketName, filename).toExternalForm();
 
-            response.setBody(b64);
-            response.setIsBase64Encoded(true);
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.setStatusCode(200);
 
-            return response;
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        response.setHeaders(headers);
 
-    }
+        response.setBody("{ \"url\": \"" + objectURL + "\" }");
 
-    private String encodeFileToBase64Binary(String fileName) throws IOException {
-        File file = new File(fileName);
-        byte[] bytes = loadFile(file);
-        byte[] encoded = Base64.getEncoder().encode(bytes);
-        String encodedString = new String(encoded);
-        return encodedString;
-    }
-
-    private static byte[] loadFile(File file) throws IOException {
-        InputStream is = new FileInputStream(file);
-
-        long length = file.length();
-        if (length > Integer.MAX_VALUE) {
-            // File is too large
-        }
-        byte[] bytes = new byte[(int) length];
-
-        int offset = 0;
-        int numRead = 0;
-        while (offset < bytes.length
-                && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-            offset += numRead;
-        }
-
-        if (offset < bytes.length) {
-            throw new IOException("Could not completely read file " + file.getName());
-        }
-
-        is.close();
-        return bytes;
+        return response;
     }
 }
